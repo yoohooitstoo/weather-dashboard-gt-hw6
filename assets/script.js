616f48257f6585c938587775ca88f84d0a462d6a
yoohooitstoo <stumanscott@gmail.com>
2020-06-17 21:14:57 -0400

    got local storage to store and persist

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,53 @@
 $(document).ready(function () {
+    
+    
+        // If there is something in local display on the page
+        if(localStorage.getItem("searchedCityNames")) {
+         let arrayOfCityNames = JSON.parse(localStorage.getItem("searchedCityNames"));
+            // Need a for loop to go through the array
+        for (var i = 0; i < arrayOfCityNames.length; i++) {
+            var userInput = $("<li>", {
+                class: "list-group-item",
+                click: function () {
 
-    var arrayOfCityNames = [];
+                    displayWeatherInfo(event, this);    
+                }
+            })
+            // List of user generated cities
+            userInput.addClass("newCity");
+            // Grabbing all the attributes attached city name at that index
+            userInput.attr("data-name", arrayOfCityNames[i]);
+            // 
+            userInput.text(arrayOfCityNames[i]);
+            // Change to prepend when ready
+            $("#cityList").append(userInput);
+         }
+   }
+    
+        
+
+   
     // button function for search
     $("#newCity").on("click", function (event) {
-        $(".emptyDiv").empty();
+        $(".emptyDiv").detach();
         event.preventDefault();
-
-        // City object
+        // sets the city and removes any dead spaces
         var city = $("#cityInput").val().trim();
-        console.log(city);
-        // pushes city into city array
-        arrayOfCityNames.push(city);
-        // hopefully storing in local on the 
-        // localStorage.setItem(cityNames, cityNames);
-        // localStorage.getItem(cityNames);
-        // renderListItems();
-        // for (var i = 0; i < cityName.length; i++) {
+        // console.log(city);
+        //localStorage is empty so is NewcityNames
+        if (!localStorage.searchedCityNames){
+            let arrayOfCityNames = []
+            arrayOfCityNames.push(city)
+            // Adding to the array is empty
+            localStorage.setItem("searchedCityNames", JSON.stringify(arrayOfCityNames));
+          } else {
+            //  If there is already cities keeping adding
+            let arrayOfCityNames = JSON.parse(localStorage.getItem("searchedCityNames"));
+            arrayOfCityNames.push(city);
+            localStorage.setItem("searchedCityNames", JSON.stringify(arrayOfCityNames));
+          };
+        
+        //   The list of generated cities by user
         var userInput = $("<li>", {
             class: "list-group-item",
             click: function () {
@@ -29,7 +61,7 @@ $(document).ready(function () {
             }
 
         })
-        // 
+        // Adding style to city
         userInput.addClass("newCity");
         userInput.attr("data-name", city);
         userInput.text(city)
@@ -40,6 +72,7 @@ $(document).ready(function () {
     function displayWeatherInfo(event, element) {
        
         // console.log(event, element);
+        // My own generated Api key 
         var APIKey = "f7260b0580dc94670030951a250ac329";
         var city = $(element).text();
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
@@ -76,7 +109,7 @@ $(document).ready(function () {
                 $("#cityUvIndex").append("UV Index: ").append($("#cityUvIndexNumbers").append(response2.value));
 
                 var cityUvIndex = parseInt(parseFloat(response2.value));
-
+                //Setups up our 
                 if (cityUvIndex < 3) {
                     $("#cityUvIndexNumbers").addClass("green");
                 } else if (cityUvIndex < 7) {
@@ -95,6 +128,9 @@ $(document).ready(function () {
             }).then(function (response3) {
                 console.log(response3);
                 // console.log(response3.list[0].dt_txt);
+                // Figure out where to find the date and card info
+
+                //Card one 
                 $("#cityDayOneDate").append(response3.list[0].dt_txt.split(" ")[0]);
                 $("#cityDayOneTemp").append("Temp: " + response3.list[0].main.temp + "°F");
                 $("#cityDayOneHumd").append("Humidity: " + response3.list[0].main.humidity+ "%");
@@ -102,6 +138,8 @@ $(document).ready(function () {
                 var cityIconOneUrl = "http://openweathermap.org/img/w/" + cityIconOne + ".png";
                 $("#cityDayOneIcon").attr("src", cityIconOneUrl);
 
+
+                //Card two
                 $("#cityDayTwoDate").append(response3.list[8].dt_txt.split(" ")[0]);
                 $("#cityDayTwoTemp").append("Temp: " + response3.list[8].main.temp + "°F");
                 $("#cityDayTwoHumd").append("Humidity: " + response3.list[8].main.humidity+ "%");
@@ -109,13 +147,16 @@ $(document).ready(function () {
                 var cityIconTwoUrl = "http://openweathermap.org/img/w/" + cityIconTwo + ".png";
                 $("#cityDayTwoIcon").attr("src", cityIconTwoUrl);   
 
+
+                //Card three
                 $("#cityDayThreeDate").append(response3.list[16].dt_txt.split(" ")[0]);
                 $("#cityDayThreeTemp").append("Temp: " + response3.list[16].main.temp + "°F");
                 $("#cityDayThreeHumd").append("Humidity: " + response3.list[16].main.humidity+ "%");
                 var cityIconThree = response3.list[16].weather[0].icon;
                 var cityIconThreeUrl = "http://openweathermap.org/img/w/" + cityIconThree + ".png";
                 $("#cityDayThreeIcon").attr("src", cityIconThreeUrl);   
-
+                
+                //Card four
                 $("#cityDayFourDate").append(response3.list[24].dt_txt.split(" ")[0]);
                 $("#cityDayFourTemp").append("Temp: " + response3.list[24].main.temp + "°F");
                 $("#cityDayFourHumd").append("Humidity: " + response3.list[24].main.humidity+ "%");
@@ -123,15 +164,20 @@ $(document).ready(function () {
                 var cityIconFourUrl = "http://openweathermap.org/img/w/" + cityIconFour + ".png";
                 $("#cityDayFourIcon").attr("src", cityIconFourUrl);
                 
+                //Card five
                 $("#cityDayFiveDate").append(response3.list[32].dt_txt.split(" ")[0]);
                 $("#cityDayFiveTemp").append("Temp: " + response3.list[32].main.temp + "°F");
                 $("#cityDayFiveHumd").append("Humidity: " + response3.list[32].main.humidity+ "%");
                 var cityIconFive = response3.list[32].weather[0].icon;
                 var cityIconFiveUrl = "http://openweathermap.org/img/w/" + cityIconFive + ".png";
-                $("#cityDayFiveIcon").attr("src", cityIconFiveUrl);   
+                $("#cityDayFiveIcon").attr("src", cityIconFiveUrl);
+
                 
             });
         })
+
+        
+
     }
 
 
